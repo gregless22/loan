2d771e5d937d9530b6f327b3562c46f1319a19f9
Greg Connolly <Gregoryconnolly@gmail.com>
2019-11-29 21:18:44 +1000

    added json database

---
+++ database/json.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func openJSON() {
+	jsonFile, err := os.Open("loan.json")
+	defer jsonFile.Close()
+	// handle the err
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Read the JSON
+	byte, _ := ioutil.ReadAll(jsonFile)
+
+	//initialise the loans array
+	var loans Loans
+
+	// unmarshal the bytes into the struct Loan
+	err = json.Unmarshal(byte, &loans)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//test
+	for i := 0; i < len(loans.Loans); i++ {
+		fmt.Println(loans.Loans[i])
+	}
+
+}
--- main.go
+++ main.go
@@ -1,50 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 func main() {
 	http.HandleFunc("/gql", handler)
-	openJSON()
+	db.openJSON()
 	log.Fatal(http.ListenAndServe(":7777", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "HI welcome to the servder")
-}
-
-func openJSON() {
-	jsonFile, err := os.Open("loan.json")
-	defer jsonFile.Close()
-	// handle the err
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Read the JSON
-	byte, _ := ioutil.ReadAll(jsonFile)
-
-	//initialise the loans array
-	var loans Loans
-
-	// unmarshal the bytes into the struct Loan
-	err = json.Unmarshal(byte, &loans)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	//test
-	for i := 0; i < len(loans.Loans); i++ {
-		fmt.Println(loans.Loans[i])
-	}
-
 }
 
 // create the structs to hold the loan
