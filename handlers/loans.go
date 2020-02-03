4b36c2e265fbaf70f6d4c051f14b31dff4faa308
Greg Connolly <Gregoryconnolly@gmail.com>
2020-02-04 08:57:54 +1100

    added loans module

---
+++ handlers/loans.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"github.com/gregless22/loan/database"
+)
+
+// connect to the database for each function
+func openDB() {
+	database.OpenJSON()
+}
+
+// TODO Create
+
+// TODO Read
+
+// TODO Update
+
+// TODO Delete
--- main.go
+++ main.go
@@ -4,16 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gregless22/loan/database"
 )
 
 func main() {
-	http.HandleFunc("/gql", handler)
-	database.OpenJSON()
+	http.HandleFunc("/", handler)
+
 	log.Fatal(http.ListenAndServe(":7777", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HI welcome to the servder")
+	// return the handler object
+	fmt.Fprintf(w, "Hi welcome to the servder")
 }
